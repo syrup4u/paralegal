b59391534839fece8c9e99720e4bb3d063ef4166
syrup4u <jintliuuuuu@gmail.com>
2026-04-11 15:30:25 -0400

    Add an exp script and a doc

---
+++ guide/deletion-policy/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Test 1
+TARGET_DIR="../bug-db-example"
+POLICY_FILE="policy.txt"
+
+# Run
+sed -i 's/let p = Path::new(".*");/let p = Path::new("'$POLICY_FILE'");/' build.rs
+cargo run -- --external-annotations external-annotations.toml $TARGET_DIR
